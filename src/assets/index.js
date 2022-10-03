076524bbda1df53da16116a1d24e65618eb59230
gtagamermods <coolsagnik2005@gmail.com>
2022-10-03 11:28:09 +0530

    Starters

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,7 @@ import shield from "./Shield.svg";
 import star from "./Star.svg";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import arrowUp from "./arrow-up.svg";
-import facebook from "./facebook.svg";
-import instagram from "./instagram.svg";
-import twitter from "./twitter.svg";
-import heroImg from "./Saly-10.png";
+import heroImg from "./Saly-16.png";
 import page404 from "./404page.svg";
 
 export {
@@ -16,10 +12,6 @@ export {
   star,
   menu,
   close,
-  arrowUp,
-  facebook,
-  instagram,
-  twitter,
   heroImg,
   page404
 };
